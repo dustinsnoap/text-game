f938051346a80fefe34b63dc71009634a6b04fb7
Dustin Snoap <afrobacon@gmail.com>
2019-03-28 20:37:30 -0400

    Completed cetner.js

--- src/components/main/center/center.js
+++ src/components/main/center/center.js
@@ -1,18 +1,63 @@
-import React, { Component } from 'react'
+import React from 'react'
 import styled from 'styled-components'
 
 const Center = ({player}) => {
     return (
         <CENTER>
-            <div className='location'>
-                <h3>You currently live in</h3>
-                <span>{player.city}.</span>
-            </div>
-            <div className='cash-turns'>
-                <div className='cash'>
-                    <h3>Money:</h3>
-                    <span>{player.cash}</span>
+            <div className='row'>
+                <div className='col'>
+                    <pre>You currently live in</pre>
+                    <span className='bold center'>Las Vegas</span>
                 </div>
+            </div>
+            <div className='row'>
+                <div className='col'>
+                    <span className='big underline center'>Money:</span>
+                    <span className='huge center'>${player.cash}</span>
+                </div>
+                <div className='col'>
+                    <span className='big underline center'>Turns:</span>
+                    <span className='huge center'>300</span>
+                </div>
+            </div>
+            <div className='row'>
+                <pre>You have been a member of the alliance</pre>
+                <span className='link'>{player.alliance}</span>
+                <pre>for two weeks.</pre>
+            </div>
+            <div className='row'>
+                <div className='col'>
+                    <span className='underline center'>Whore payout:</span>
+                    <div className='row'>
+                        <pre>%</pre>
+                        <input type='number' value={player.payout}/>
+                        <button>SET</button>
+                    </div>
+                    <span>Whore Happiness: {player.whoreHappiness}</span>
+                    <span>Thug Happiness: {player.thugHappiness}</span>
+                </div>
+            </div>
+            <div className='row'>
+                <div className='attacks'>
+                    <pre>Attacks:</pre>
+                    <pre>In:</pre>
+                    <span className='red'>{player.attacksIn}</span>
+                    <pre>Out:</pre>
+                    <span className='red'>{player.attacksOut}</span>
+                </div>   
+            </div>
+            <div className='row'>
+                <div className='assets'>
+                    {player.assets.map(asset => 
+                        <React.Fragment>
+                            <span className='purple'>{asset.name}</span>
+                            <pre>{asset.value}</pre>
+                        </React.Fragment>
+                        )}
+                </div>
+            </div>
+            <div className='row'>
+                <button class='link'>Open Console</button>
             </div>
         </CENTER>
     )
@@ -21,5 +66,61 @@ const Center = ({player}) => {
 export default Center
 
 const CENTER = styled.div`
+    display: flex;
+    flex-direction: column;
+    font-family: verdana;
+    font-size: 1.3rem;
+    text-align: center;
 
+    .big {font-size: 1.6rem}
+    .bold {font-weight: 700}
+    .center {text-align: center}
+    .huge {font-size: 1.92rem}
+    .link { color: #09f}
+    .red {color: #f00}
+    .purple {color: #80f}
+    .underline {text-decoration: underline}
+
+    input {
+        margin: 0 0.5rem;
+        width: 40px;
+    }
+    button {
+        border: none;
+        background-color: transparent;
+        color: #fff;
+        font-weight: 700;
+    }
+    .row {
+        display: flex;
+        justify-content: center;
+        margin: 0.5rem 0;
+    }
+    .col {
+        display: flex;
+        flex-direction: column;
+        margin: 0 0.5rem;
+        width: fit-content;
+    }
+    .assets {
+        display: grid;
+        grid-template-columns: repeat(4, auto);
+        grid-gap: 0.5rem;
+        width: fit-content;
+        padding: 1rem 1rem;
+    }
+    .attacks {
+        display: flex;
+        span, pre {
+            margin: 0 0.25rem;
+        }
+    }
+    span {
+        width: 100%;
+        text-align: left;
+    }
+    pre {
+        text-align: right;
+        width: 100%;
+    }
 `
--- src/components/main/main.js
+++ src/components/main/main.js
@@ -9,10 +9,32 @@ class Main extends Component {
         this.state = {
             username: '',
             id: 1042,
+            alliance: 'allyname',
             rank: '7',
             networth: '1,967,781,500',
             title: 'HELL HATH NO',
             city: 'Las Vegas',
+            cash: 1333337,
+            turns: 300,
+            payout: 1,
+            whoreHappiness: 100,
+            thugHappiness: 100,
+            attacksIn: 0,
+            attacksOut: 0,
+            assets: [
+                {name: 'Whores', value: 236281},
+                {name: 'Thugs', value: 46124162},
+                {name: 'Condoms', value: 2362812},
+                {name: 'Weapons', value: 0},
+                {name: 'Crack', value: 462462},
+                {name: 'Beer', value: 46124162},
+                {name: 'Medicine', value: 240000},
+                {name: 'Low-riders', value: 1},
+                {name: 'Pistols', value: 0},
+                {name: 'Shotguns', value: 0},
+                {name: 'MP5s', value: 0},
+                {name: 'AK-47s', value: 0}
+            ]
         }
     }
     componentDidMount() {
